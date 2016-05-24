75497e01424431d2b48174364ac74ec04bb191cb
${USER_NAME} <${EMAIL}>
2016-05-24 09:00:14 -0700

    Now can persist polls. Still doesnt go to poll.

--- main.go
+++ main.go
@@ -4,28 +4,30 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", makePoll)
-	http.HandleFunc("/newpoll/", newPoll)
-	http.HandleFunc("/poll/", viewPoll)
-	http.HandleFunc("/poll/r/", pollResults)
+	r := mux.NewRouter()
+	r.HandleFunc("/", makePoll)
+	r.HandleFunc("/newpoll/", newPoll)
+	r.HandleFunc("/poll/{id:[0-9]+}", viewPoll).Methods("GET")
+	r.HandleFunc("/poll/{id:[0-9]+}/r/", pollResults).Methods("GET")
+	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	t := template.New("baseTemplate")    // Create a template.
-	t, _ = t.ParseFiles("question.html") // Parse template file.
-	aPoll := Poll{
-		Question: "What are you doing for lunch?",
-		Answers:  []string{"x", "y"},
-	}
-	t.ExecuteTemplate(w, "main", aPoll) // merge.
-}
-
 func viewPoll(w http.ResponseWriter, req *http.Request) {
-
+	t := template.New("baseTemplate")     // Create a template.
+	t, _ = t.ParseFiles("view/poll.html") // Parse template file.
+	pollId, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		return
+	}
+	thePoll := polls[pollId]
+	t.ExecuteTemplate(w, "poll", thePoll) // merge.
 }
 
 func pollResults(w http.ResponseWriter, req *http.Request) {
@@ -41,11 +43,13 @@ func newPoll(w http.ResponseWriter, req *http.Request) {
 	t, _ = t.ParseFiles("view/poll.html") // Parse template file.
 	aPoll := Poll{
 		Question: req.PostFormValue("question"),
-		Answers:  req.Form["option"],
+		Answers:  removeBlanks(req.Form["option"]),
 	}
+	storePoll(aPoll)
 	t.ExecuteTemplate(w, "poll", aPoll) // merge.
 
 }
+
 func makePoll(w http.ResponseWriter, req *http.Request) {
 	t := template.New("main")              // Create a template.
 	t, _ = t.ParseFiles("view/index.html") // Parse template file.
@@ -58,4 +62,21 @@ type Poll struct {
 	Multiselect bool
 	PerIp       bool
 	PerBrowser  bool
+}
+
+func removeBlanks(theStrings []string) []string {
+	nonblank := []string{}
+	for _, aString := range theStrings {
+		if len(aString) > 0 {
+			nonblank = append(nonblank, aString)
+		}
+	}
+	return nonblank
+}
+
+var polls []Poll = []Poll{}
+
+func storePoll(thePoll Poll) int {
+	polls = append(polls, thePoll)
+	return len(polls) - 1
 }
