0361fb4ebddca2109dfffd8761f8070326700fbf
${USER_NAME} <${EMAIL}>
2016-05-23 22:00:11 -0700

    got it sort of working.

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", makePoll)
+	http.HandleFunc("/newpoll/", newPoll)
 	http.HandleFunc("/poll/", viewPoll)
 	http.HandleFunc("/poll/r/", pollResults)
 	http.ListenAndServe(":8080", nil)
@@ -22,16 +24,32 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "main", aPoll) // merge.
 }
 
-func viewPoll(rw http.ResponseWriter, req *http.Request) {
+func viewPoll(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func pollResults(rw http.ResponseWriter, req *http.Request) {
+func pollResults(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func makePoll(rw http.ResponseWriter, req *http.Request) {
+func newPoll(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		fmt.Fprintf(w, "Nope dip")
+	}
+	t := template.New("baseTemplate")     // Create a template.
+	t, _ = t.ParseFiles("view/poll.html") // Parse template file.
+	aPoll := Poll{
+		Question: req.PostFormValue("question"),
+		Answers:  req.Form["option"],
+	}
+	t.ExecuteTemplate(w, "poll", aPoll) // merge.
 
+}
+func makePoll(w http.ResponseWriter, req *http.Request) {
+	t := template.New("main")              // Create a template.
+	t, _ = t.ParseFiles("view/index.html") // Parse template file.
+	t.ExecuteTemplate(w, "main", nil)      // merge.
 }
 
 type Poll struct {
