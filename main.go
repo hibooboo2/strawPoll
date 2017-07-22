5d20bb86b8deb00dda9a083cea5b99c3ee03c82e
James Jeffrey <wizardofmath@gmail.com>
2017-07-21 22:06:59 -0700

    Update main.go

--- main.go
+++ main.go
@@ -112,7 +112,7 @@ func votePoll(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		panic("shit")
 	}
-	voted := polls.Vote(pollID, req.PostFormValue("Answer"), req.RemoteAddr)
+	voted := polls.Vote(pollID, req.PostFormValue("Answer"), strings.Split(req.RemoteAddr)[0])
 	if !voted {
 		log.Println("Unable to vote. Probably already voted: " + req.RemoteAddr)
 		http.Redirect(w, req, fmt.Sprintf("/poll/%d/r/?alreadyVoted=true", pollID), http.StatusSeeOther)
